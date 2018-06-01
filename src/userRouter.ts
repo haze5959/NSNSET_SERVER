19af889d5a16383649c08fe86f0df6c43a6ab8ac
haze5959 <haze5959@naver.com>
2018-06-01 10:34:20 +0900

    3242

--- src/userRouter.ts
+++ src/userRouter.ts
@@ -44,9 +44,9 @@ router.get('/', async (ctx) => {
     var sort = param['sort']?param['sort']:'stNum';
     let count = param['count']?param['count']:5;
 
-    if (sort == "stNum") {  //학번순
-      sort = 'STUDENT_NUM';
-    } else {  //잉여 포인트순
+    if (sort == "update") {  //활동순
+      sort = 'RECENT_DATE';
+    } else {  //잉여 포인트순 rank
       sort = 'POINT';
     }
 
@@ -67,7 +67,258 @@ router.get('/', async (ctx) => {
     });
   }
 
+});
+
+/**
+ * POST
+ */
+router.post('/', async (ctx) => {  
+
+  const param = ctx.body;
+  // console.log("[ctx.params] : " + JSON.stringify(param));
+  const payload = param['payload'];
+
+  if(!payload){
+    ctx.body = "페이로드가 없습니다.";
+    return false;
+  }
+
+  let postId = payload.postId;
+  // let studentNum = payload.studentNum;
+  let userId = payload.userId;
+  // let userName = payload.userName;
+  // let userImg = payload.userImg;
+  let commentEmo = payload.emoticon?payload.emoticon:"";
+
+  let commentBody = payload.comment?payload.comment:"";
   
+  const db = new oracleDB();
+  let connection = await db.getConnection()
+  .then(con => {
+    return con;
+  }).catch(err => {
+    ctx.body = {
+      result: false,
+      message: err.message
+    };
+    console.error("[error] : " + JSON.stringify(ctx.body));
+    return null;
+  });
+
+  if(!connection){
+    //통신 종료
+    return false;
+  }
+  
+  //코맨트 올리기================================================
+  await connection.execute(`INSERT INTO COMMENTS 
+  (COMMENT_BODY, COMMENT_ID, COMMENT_DATE, USER_ID, EMOTICON, POST_ID, GOOD) 
+  VALUES (:commentBody, SEQ_COMMENT_ID.NEXTVAL, SYSDATE, :userId, :commentEmo, :postId, 0)`, 
+  { commentBody: commentBody, userId: userId, commentEmo: commentEmo, postId: postId })
+  .then(result => {
+    // console.log("[response1] : " + JSON.stringify(result));
+  }, err => {
+    connection.rollback();
+    connection.release();
+    throw err;
+  }).catch(err => {
+    ctx.body = {
+      result: false,
+      message: err.message
+    };
+    console.error("[error] : " + ctx.body);
+  });
+  //================================================================
+
+  //게시글 댓글 수 올리기================================================
+  await connection.execute(`UPDATE POSTS SET 
+  COMMENT_COUNT = COMMENT_COUNT + 1
+  WHERE POST_ID = :postId`, { postId: postId })
+  .then(result => {
+    // console.log("[response2] : " + JSON.stringify(result));
+    connection.release();
+    ctx.body = {
+      result: true,
+      message: result
+    };
+  }, err => {
+    throw err;
+
+  }).catch(err => {
+    connection.rollback();
+    connection.release();
+    ctx.body = {
+      result: false,
+      message: err.message
+    };
+    console.error("[error] : " + ctx.body);
+  });
+  //================================================================
+});
+
+/**
+ * PUT
+ */
+router.put('/', async (ctx) => {  
+  const param = ctx.body;
+  // console.log("[ctx.params] : " + JSON.stringify(param));
+  
+  if(!cognitoJWT.check(param['accessToken']?param['accessToken']:'')){  //토큰 검증 실패
+    console.error("토큰 검증 실패");
+    ctx.body = "토큰 검증 실패";
+    return false;
+  } 
+  const payload = param['payload'];
+
+  if(!payload){
+    ctx.body = "페이로드가 없습니다.";
+    return false;
+  }
+
+  let postId = payload.postId;
+  let userId = payload.userId;
+  let isGood:boolean = payload.isGood;
+
+  var goodBadQuery = "BAD = BAD + 1";
+  if(isGood){
+    goodBadQuery = "GOOD = GOOD + 1";
+  }
+
+  const db = new oracleDB();
+  let connection = await db.getConnection()
+  .then(con => {
+    return con;
+  }).catch(err => {
+    ctx.body = {
+      result: false,
+      message: err.message
+    };
+    console.error("[error] : " + JSON.stringify(ctx.body));
+    return null;
+  });
+
+  if(!connection){
+    //통신 종료
+    return false;
+  }
+  
+  //해당 게시글 평가================================================
+  await connection.execute(`UPDATE POSTS SET ${goodBadQuery} WHERE POST_ID = :postId`, 
+  { postId: postId })
+  .then(result => {
+    //성공
+  }, err => {
+    throw err;
+
+  }).catch(err => {
+    connection.rollback();
+    connection.release();
+    ctx.body = {
+      result: false,
+      message: err.message
+    };
+    console.error("[error] : " + ctx.body);
+    return false;
+  });
+  //================================================================
+
+  //해당 유저 점수 등록================================================
+  await connection.execute(`UPDATE USERS SET POINT = POINT + ${evalPoint} WHERE USER_ID = :userId`, 
+  { userId: userId })
+  .then(result => {
+    // console.log("[response2] : " + JSON.stringify(result));
+    connection.release();
+    ctx.body = {
+      result: true,
+      message: result
+    };
+  }, err => {
+    throw err;
+    
+  }).catch(err => {
+    connection.rollback();
+    connection.release();
+    ctx.body = {
+      result: false,
+      message: err.message
+    };
+    console.error("[error] : " + ctx.body);
+    return false;
+  });
+  //================================================================
+});
+
+/**
+ * DELETE
+ */
+router.delete('/', async (ctx) => {  
+
+  const param = ctx.request.query;
+  // console.log("[ctx.params] : " + JSON.stringify(param));
+  let commentId = param.commentId;
+  let postId = param.postId;
+
+  const db = new oracleDB();
+  let connection = await db.getConnection()
+  .then(con => {
+    return con;
+  }).catch(err => {
+    ctx.body = {
+      result: false,
+      message: err.message
+    };
+    console.error("[error] : " + JSON.stringify(ctx.body));
+    return null;
+  });
+
+  if(!connection){
+    //통신 종료
+    return false;
+  }
+
+  //댓글 삭제================================================
+  await connection.execute(`DELETE FROM COMMENTS WHERE COMMENT_ID = :commentId`, 
+  { commentId: commentId })
+  .then(result => {
+    //성공
+  }, err => {
+    throw err;
+
+  }).catch(err => {
+    connection.rollback();
+    connection.release();
+    ctx.body = {
+      result: false,
+      message: err.message
+    };
+    console.error("[error] : " + ctx.body);
+  });
+  //================================================================
+
+  //게시글 댓글 수 빼기================================================
+  await connection.execute(`UPDATE POSTS SET 
+  COMMENT_COUNT = COMMENT_COUNT - 1
+  WHERE POST_ID = :postId`, { postId: postId })
+  .then(result => {
+    // console.log("[response2] : " + JSON.stringify(result));
+    connection.release();
+    ctx.body = {
+      result: true,
+      message: result
+    };
+  }, err => {
+    throw err;
+
+  }).catch(err => {
+    connection.rollback();
+    connection.release();
+    ctx.body = {
+      result: false,
+      message: err.message
+    };
+    console.error("[error] : " + ctx.body);
+  });
+  //================================================================
 });
 
 export default router.routes();
